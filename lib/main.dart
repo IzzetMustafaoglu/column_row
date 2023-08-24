4b3e95a3410f04207b5d0a20bf291071360a8aa3
IzzetMustafaoglu <izzetmustafaoglu00@gmail.com>
2023-08-24 08:22:50 +0300

    dorduncu_row_commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class BenimUyg extends StatelessWidget{
       home: Scaffold(
         backgroundColor: Colors.black,
         body: SafeArea(
-          child: Column(
+          child: Row(
         //    mainAxisAlignment: MainAxisSize.min,         //Daha fazla yer kaplama
         //    mainAxisAlignment: MainAxisAlignment.start,  // Dikey yapar(varsayılan)
         //    mainAxisAlignment: MainAxisAlignment.end,    //En alta çeker
@@ -22,32 +22,50 @@ class BenimUyg extends StatelessWidget{
         //    crossAxisAlignment: CrossAxisAlignment.end,
         //    crossAxisAlignment: CrossAxisAlignment.center,  // Ortaya al
             crossAxisAlignment: CrossAxisAlignment.stretch,   // Yatayda ekranı doldur
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
             Container(
               color: Colors.white,
-              width: 200,
+        //      width: 200,
               height: 100,
               child: Text("1. Container"),
             ),
               SizedBox(    // iki Container arasında boşluk bırakır
                 height: 30,
+                width: 20,
               ),
             Container(
         //      margin: EdgeInsets.symmetric(vertical: 20), // Diğerleri ile arasında boşluk bırakır
-              width: 200,
+        //      width: 200,
               height: 100,
               color: Colors.pinkAccent,
               child: Text("2. Container"),
             ),
             SizedBox(    // iki Container arasında boşluk bırakır
               height: 30,
+              width: 20,
             ),
             Container(
-              width: 200,
+        //      width: 200,
               height: 100,
               color: Colors.lightBlue,
               child: Text("3. Container"),
             ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    height: 50,
+                    width: 50,
+                    color: Colors.green,
+                  ),
+                  Container(
+                    height: 50,
+                    width: 50,
+                    color: Colors.yellow,
+                  ),
+                ],
+              ),
 
               // Container(
               //   width: double.infinity,   // Ekran el verdikçe en dibe ver
